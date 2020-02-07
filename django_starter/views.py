b06f0be82a93f80a625ae01e216ebd636411e1be
Aaron <mohammad.chegini@hotmail.com>
2020-02-07 22:46:57 +0330

    handling a simple addition in the backend of the website

--- django_starter/views.py
+++ django_starter/views.py
@@ -5,4 +5,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello world!")
+    return render(request, 'index.html', {'name': 'Lardass'})
+
+
+def result(request):
+    res = int(request.GET['num1']) + int(request.GET['num2'])
+    return render(request, 'result.html', {'result': res})
