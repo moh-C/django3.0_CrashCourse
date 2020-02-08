8f33c4b9b8b8b2ccbfd261731e5dfea1de030554
Aaron <mohammad.chegini@hotmail.com>
2020-02-08 21:47:23 +0330

    index is now added

--- django_starter/urls.py
+++ django_starter/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('/', views.home, name='home'),
-    path('add_something', views.result, name='Add something')
+
 ]
--- django_starter/views.py
+++ django_starter/views.py
@@ -3,11 +3,3 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-
-def home(request):
-    return render(request, 'index.html', {'name': 'Lardass'})
-
-
-def result(request):
-    res = int(request.POST['num1']) + int(request.POST['num2'])
-    return render(request, 'result.html', {'result': res})
