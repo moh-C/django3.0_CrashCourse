ef34603e1fc219befbbea38c85cae658e2dc7517
Aaron <mohammad.chegini@hotmail.com>
2020-02-09 12:33:54 +0330

    images are now dynamic

--- django_starter/views.py
+++ django_starter/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Destination
 
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'index.html', {'price': 700})
+    dest1 = Destination()
+    dest1.price = 700
+    dest1.name = 'Tehran'
+    dest1.desc = 'A lovely place!'
+    dest1.img = 'destination_1.jpg'
+
+    dest2 = Destination()
+    dest2.price = 678
+    dest2.name = 'LA'
+    dest2.desc = 'A superb place!'
+    dest2.img = 'destination_2.jpg'
+
+    dest3 = Destination()
+    dest3.price = 594
+    dest3.name = 'Istanbul'
+    dest3.desc = 'A dull place!'
+    dest3.img = 'destination_3.jpg'
+
+    dests = [dest1, dest2, dest3]
+
+    return render(request, 'index.html', {'dests': dests})
