e5d2bb8331f9a7b94470b314aedcbaf682c4303a
Aaron <mohammad.chegini@hotmail.com>
2020-02-09 12:40:06 +0330

    special offer is now handled

--- django_starter/views.py
+++ django_starter/views.py
@@ -11,18 +11,21 @@ def index(request):
     dest1.name = 'Tehran'
     dest1.desc = 'A lovely place!'
     dest1.img = 'destination_1.jpg'
+    dest1.offer = True
 
     dest2 = Destination()
     dest2.price = 678
     dest2.name = 'LA'
     dest2.desc = 'A superb place!'
     dest2.img = 'destination_2.jpg'
+    dest2.offer = False
 
     dest3 = Destination()
     dest3.price = 594
     dest3.name = 'Istanbul'
     dest3.desc = 'A dull place!'
     dest3.img = 'destination_3.jpg'
+    dest3.offer = False
 
     dests = [dest1, dest2, dest3]
 
